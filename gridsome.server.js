76ccd6c01366433426bb7d25707929dfc2f1a06f
Abdessalam BENHARIRA <belmehelabdessalam.benharira@ynov.com>
2020-05-06 12:56:01 +0200

    feat: add posts and products from wp api

--- gridsome.server.js
+++ gridsome.server.js
@@ -1,16 +1,40 @@
-// Server API makes it possible to hook into various parts of Gridsome
-// on server-side and add custom data to the GraphQL data layer.
-// Learn more: https://gridsome.org/docs/server-api/
+const { API_URL_WP } = require("./src/config");
 
-// Changes here require a server restart.
-// To restart press CTRL + C in terminal and run `gridsome develop`
+const axios = require("axios");
 
-module.exports = function (api) {
-  api.loadSource(({ addCollection }) => {
-    // Use the Data Store API here: https://gridsome.org/docs/data-store-api/
-  })
+const normalizeWordPressPost = (item) => {
+  return {
+    id: item.ID || item.id,
+    title: item.post_title,
+    slug: item.post_name,
+    date: item.post_date,
+    content: item.post_content,
+    type: item.post_type,
+    customFields: item.custom_fields,
+  };
+};
 
-  api.createPages(({ createPage }) => {
-    // Use the Pages API here: https://gridsome.org/docs/pages-api/
-  })
-}
+const getPosts = async (actions) => {
+  const { data } = await axios.get(`${API_URL_WP}/posts`);
+  const collection = actions.addCollection("Post");
+  for (const item of data) {
+    collection.addNode(normalizeWordPressPost(item));
+  }
+};
+
+const getProducts = async (actions) => {
+  const { data } = await axios.get(`${API_URL_WP}/products`);
+  const collection = actions.addCollection("Product");
+  for (const item of data) {
+    collection.addNode(normalizeWordPressPost(item));
+  }
+};
+
+module.exports = (api) => {
+  api.loadSource(async (actions) => {
+    await getPosts(actions);
+    await getProducts(actions);
+  });
+
+  api.createPages(({ createPage }) => {});
+};
---
+++ src/templates/Product.vue
@@ -0,0 +1,60 @@
+<template>
+  <Layout>
+    <div class="product-title">
+      <h1 class="product-title__text">
+        {{ $page.product.title }}
+      </h1>
+    </div>
+
+    <div class="product content-box">
+      <div class="product__header">
+        <g-image
+          alt="Cover image"
+          v-if="$page.product.customFields.image"
+          :src="$page.product.customFields.image"
+        />
+      </div>
+
+      <div class="product__content" v-html="$page.product.content" />
+
+      <div class="product__footer"></div>
+    </div>
+
+    <div class="product-comments"></div>
+  </Layout>
+</template>
+
+<script>
+export default {
+  components: {},
+  metaInfo() {
+    return {
+      title: this.$page.product.title,
+      meta: [
+        {
+          name: "description",
+          content: this.$page.product.description,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<page-query>
+query Product ($id: ID!) {
+    product: product (id: $id) {
+        id
+        title
+        content
+        slug
+        date
+        type
+        customFields {
+            image
+        }
+    }
+}
+</page-query>
+
+<style></style>
