79756d641d386eac25d038f70c173694c998708d
Abdessalam BENHARIRA <belmehelabdessalam.benharira@ynov.com>
2020-05-24 16:54:01 +0200

    feat: add success contact page

---
+++ src/pages/ContactSubmitSuccess.vue
@@ -0,0 +1,19 @@
+<template>
+  <Layout>
+    <h1>Contact submit success!</h1>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Contact submit success!',
+    meta: [
+      {
+        name: 'description',
+        content: 'smatwatch website',
+      },
+    ],
+  },
+};
+</script>
